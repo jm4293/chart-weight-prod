e1df671509fc494b212fc321619aefe143ba504a
jm4293 <dlwoals4293@gmail.com>
2025-06-08 22:46:33 +0900

    [Style]

--- app/UserList.tsx
+++ app/UserList.tsx
@@ -104,7 +104,7 @@ export default function UserList() {
           <thead>
             <tr>
               <th>이름</th>
-              <th>년월일</th>
+              <th>생년월일</th>
               <th>등록번호</th>
             </tr>
           </thead>
