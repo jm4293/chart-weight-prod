e9c2c9b360853c810d42ec673d00674857be7b96
jm4293 <dlwoals4293@gmail.com>
2025-10-28 21:30:24 +0900

    style:

--- app/(main)/main/NoticeList.tsx
+++ app/(main)/main/NoticeList.tsx
@@ -11,9 +11,7 @@ export default function NoticeList() {
 
   const { data, isLoading, isSuccess } = useNoticeList({ page: 1 });
 
-  const handleRowClick = (noticeId: number) => {
-    router.push(`/user/notice/${noticeId}`);
-  };
+  const handleRowClick = (noticeId: number) => {};
 
   const formatDate = (dateString: Date) => {
     const formattedDate = dayjs(dateString).format('YY.MM.DD');
@@ -44,46 +42,44 @@ export default function NoticeList() {
   }
 
   return (
-    <Wrapper.SECTION>
-      <table className="min-w-full table-auto">
-        <thead>
-          <tr>
-            <th>
-              <Text.PARAGRAPH text="번호" />
-            </th>
-            <th colSpan={3}>
-              <Text.PARAGRAPH text="제목" />
-            </th>
-            <th>
-              <Text.PARAGRAPH text="등록일" />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.data.length > 0 ? (
-            data.data.map((item: INoticeModel) => (
-              <tr
-                key={item.id}
-                className="hover:bg-gray-50"
-                onClick={() => handleRowClick(item.id)}>
-                <td>
-                  <Text.PARAGRAPH text={item.id} />
-                </td>
-                <td colSpan={3}>
-                  <Text.PARAGRAPH text={item.title} />
-                </td>
-                <td>{formatDate(item.createdAt)}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={3}>
-                <Text.PARAGRAPH text="공지사항이 없습니다." />
+    <table className="min-w-full table-auto">
+      <thead>
+        <tr>
+          <th>
+            <Text.PARAGRAPH text="번호" />
+          </th>
+          <th colSpan={3}>
+            <Text.PARAGRAPH text="제목" />
+          </th>
+          <th>
+            <Text.PARAGRAPH text="등록일" />
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.data.length > 0 ? (
+          data.data.map((item: INoticeModel) => (
+            <tr
+              key={item.id}
+              className="hover:bg-gray-50"
+              onClick={() => handleRowClick(item.id)}>
+              <td>
+                <Text.PARAGRAPH text={item.id} />
               </td>
+              <td colSpan={3}>
+                <Text.PARAGRAPH text={item.title} />
+              </td>
+              <td>{formatDate(item.createdAt)}</td>
             </tr>
-          )}
-        </tbody>
-      </table>
-    </Wrapper.SECTION>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={3}>
+              <Text.PARAGRAPH text="공지사항이 없습니다." />
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
   );
 }
